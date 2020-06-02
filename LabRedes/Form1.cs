b64396a826ecb6af1a19f935810479143b0a3e93
Jurguen <jurguen111615@gmail.com>
2020-06-01 22:52:42 -0600

    Funcion calculadora

--- LabRedes/Form1.cs
+++ LabRedes/Form1.cs
@@ -16,6 +16,41 @@ namespace LabRedes
         {
             InitializeComponent();
         }
+
+        Double accHost1;
+        Double accHost2;
+        Double accHost3;
+        String ip = "";
+        public void mostrarIDRed(Double catSubNet, Double numSubNet)
+        {
+            accHost1 = 0;
+            accHost2 = 0;
+            accHost3 = 0;
+            for (int i = 0; i < numSubNet; i++)
+            {
+                if (textBox_Clase.Text.Equals("A"))
+                {
+                    ip = textBox_IP1.Text + "." + Convert.ToString(accHost1) + "." + Convert.ToString(accHost2) + "." + Convert.ToString(accHost3);
+                    accHost1 = accHost1 + catSubNet;
+                }
+                else if (textBox_Clase.Text.Equals("B"))
+                {
+                    ip = textBox_IP1.Text + "." + textBox_IP2.Text + "." + Convert.ToString(accHost2) + "." + Convert.ToString(accHost3);
+                    accHost2 = accHost2 + catSubNet;
+                }
+                else if (textBox_Clase.Text.Equals("C"))
+                {
+                    ip = textBox_IP1.Text + "." + textBox_IP2.Text + "." + textBox_IP3.Text + "." + Convert.ToString(accHost3);
+                    accHost3 = accHost3 + catSubNet;
+                }
+
+                grid.Rows.Add();
+                grid.Rows[i].Cells[0].Value = i;
+                grid.Rows[i].Cells[1].Value = ip;
+
+            }
+        }
+
         private void button_calcular_Click(object sender, EventArgs e)
         {
 
