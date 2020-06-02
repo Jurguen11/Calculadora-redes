e29baf4c62b2bc4f9492035aedbbedf03f5154ac
Stward Pérez Cabezas <stwardperezc@estudiantec.cr>
2020-06-01 23:05:44 -0600

    Mostrar rango de IPs creado

--- LabRedes/Form1.cs
+++ LabRedes/Form1.cs
@@ -51,6 +51,43 @@ namespace LabRedes
             }
         }
 
+        String ip1 = "";
+        String ip2 = "";
+        public void mostrarRango(Double numSubNet, Double catSubNet)
+        {
+            accHost1 = 0;
+            accHost2 = 0;
+            accHost3 = 0;
+
+            for (int i = 0; i < numSubNet; i++)
+            {
+                if (textBox_Clase.Text.Equals("A"))
+                {
+                    ip1 = textBox_IP1.Text + "." + Convert.ToString(accHost1) + "." + Convert.ToString(accHost2) + "." + Convert.ToString(accHost3 + 1);
+                    accHost1 = accHost1 + catSubNet;
+                    ip2 = textBox_IP1.Text + "." + Convert.ToString(accHost1 - 1) + "." + Convert.ToString(accHost2 + 255) + "." + Convert.ToString((accHost3 + 255) - 1);
+                }
+                else if (textBox_Clase.Text.Equals("B"))
+                {
+                    ip1 = textBox_IP1.Text + "." + textBox_IP2.Text + "." + Convert.ToString(accHost2) + "." + Convert.ToString(accHost3 + 1);
+                    accHost2 = accHost2 + catSubNet;
+                    ip2 = textBox_IP1.Text + "." + textBox_IP2.Text + "." + Convert.ToString(accHost2 - 2) + "." + Convert.ToString((accHost3 + 255) - 1);
+                }
+                else if (textBox_Clase.Text.Equals("C"))
+                {
+                    ip1 = textBox_IP1.Text + "." + textBox_IP2.Text + "." + textBox_IP3.Text + "." + Convert.ToString(accHost3 + 1);
+                    accHost3 = accHost3 + catSubNet;
+                    ip2 = textBox_IP1.Text + "." + textBox_IP2.Text + "." + textBox_IP3.Text + "." + Convert.ToString(accHost3 - 2);
+                }
+
+                String rangoIPs = ip1 + " -- " + ip2;
+                grid.Rows.Add();
+                grid.Rows[i].Cells[2].Value = rangoIPs;
+
+            }
+
+        }
+
         private void button_calcular_Click(object sender, EventArgs e)
         {
 
